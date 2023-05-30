7e38aca533e3469ac9fdd69ae681003ffe7090a7
Mohit Fating <77158042+MohitFating@users.noreply.github.com>
2023-05-30 23:53:58 +0530

    Create PositiveOrNegative.java

---
+++ PositiveOrNegative.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class NumberCheck {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+	      System.out.println("Enter the Number:");
+	      int a = sc.nextInt();
+		
+		if (a>=0)
+		{
+			System.out.println("The given number is Positive");
+
+		}
+		else
+		{
+			System.out.println("The given number is Negative");
+		}
+	}
+
+}
