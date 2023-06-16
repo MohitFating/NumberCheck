9d9efd53c12bb702a99f0375a0b6ceffdd751add
Mohit Fating <77158042+MohitFating@users.noreply.github.com>
2023-06-16 23:58:29 +0530

    Create DataTypesExample.java

---
+++ DataTypesExample.java
@@ -0,0 +1,33 @@
+public class DataTypesExample {
+    public static void main(String[] args) {
+        // Integer data types
+        byte byteNumber = 10;
+        short shortNumber = 20000;
+        int intNumber = 1000000;
+        long longNumber = 1234567890L;
+
+        // Floating-point data types
+        float floatNumber = 3.14f;
+        double doubleNumber = 2.71828;
+
+        // Character data type
+        char character = 'A';
+
+        // Boolean data type
+        boolean flag = true;
+
+        // String data type
+        String text = "Hello, World!";
+
+        // Output values
+        System.out.println("Byte: " + byteNumber);
+        System.out.println("Short: " + shortNumber);
+        System.out.println("Int: " + intNumber);
+        System.out.println("Long: " + longNumber);
+        System.out.println("Float: " + floatNumber);
+        System.out.println("Double: " + doubleNumber);
+        System.out.println("Character: " + character);
+        System.out.println("Boolean: " + flag);
+        System.out.println("String: " + text);
+    }
+}
